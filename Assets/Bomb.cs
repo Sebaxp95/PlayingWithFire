000456bfb79c0ecb1d6c3511a1953694760b6ab1
Sebastian <sebastianpustelnik95@gmail.com>
2018-04-27 17:31:53 +0200

    add player,  add blocks, set bomb

---
+++ Assets/Bomb.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+    public int firePower;
+    public int timeToExplode;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("Explode", timeToExplode);
+	}
+
+    void Explode()
+    {
+        Destroy(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        if (Mathf.Abs(x) >= Mathf.Abs(y))
+            y = 0;
+        else if (Mathf.Abs(x) <= Mathf.Abs(y))
+            x = 0;
+
+        rigidbody.velocity = new Vector2(x, y) * speed;
+    }
+}
